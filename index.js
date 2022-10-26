c0aaf164245cb792e6b1a423c8cdb54946db32ff
maruf21hossain <marufhossainmunna21@gmail.com>
2022-10-26 17:04:47 +0600

    some data added

--- index.js
+++ index.js
@@ -7,15 +7,34 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const allCategories = require('./data/categories.json');
+const allCourses = require('./data/allCourses.json');
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send(' Learn tech server  is running');
 })
 
-app.get('/categories', (req, res) =>{
+app.get('/categories', (req, res) => {
     res.send(allCategories);
 })
 
-app.listen( port,  () =>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == "06") {
+        res.send(allCourses)
+    }
+    else {
+        const courseCategory = allCourses.filter(course => course.category_id == id);
+        res.send(courseCategory)
+    }
+
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const courses = allCourses.find(course => course.id == id);
+    res.send(courses)
+})
+
+app.listen(port, () => {
     console.log('This server running on port', port);
 })
